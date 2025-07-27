297c9fd7fe8b105429d9443fc2f430d58d16fbf4
joshjal <JoshuaALevine3@gmail.com>
2025-07-27 19:24:22 -0400

    update git files

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,20 +1,32 @@
 return {
   "yetone/avante.nvim",
   event = "VeryLazy",
-  lazy = false,
-  version = false, -- set this to "*" if you want to always pull the latest change, false to update on release
+  version = false, -- Never set this value to "*"! Never!
   opts = {
     -- add any opts here
+    -- for example
+    provider = "claude",
+    providers = {
+      calude = {
+        endpoint = "https://api.anthropic.com",
+        model = "claude-3-7-sonnet-latest",
+      },
+    },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
   dependencies = {
-    "stevearc/dressing.nvim",
+    "nvim-treesitter/nvim-treesitter",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
     --- The below dependencies are optional,
+    "echasnovski/mini.pick", -- for file_selector provider mini.pick
+    "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
     "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+    "ibhagwan/fzf-lua", -- for file_selector provider fzf
+    "stevearc/dressing.nvim", -- for input provider dressing
+    "folke/snacks.nvim", -- for input provider snacks
     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
     "zbirenbaum/copilot.lua", -- for providers='copilot'
     {
